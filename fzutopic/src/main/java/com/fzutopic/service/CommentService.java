05d93f72f478a76c60dca1201465d508acfe7d50
sunny <2453709145@qq.com>
2020-05-01 11:12:45 +0800

    新闻类、评论类、话题类部分接口实现

--- fzutopic/src/main/java/com/fzutopic/service/CommentService.java
+++
@@ -1,7 +0,0 @@
-package com.fzutopic.service;
-
-import com.fzutopic.model.Comment;
-
-public interface CommentService {
-    Comment createComment(Comment comment);
-}
--- fzutopic/src/main/java/com/fzutopic/service/TopicService.java
+++
@@ -1,7 +0,0 @@
-package com.fzutopic.service;
-
-import com.fzutopic.model.Topic;
-
-public interface TopicService {
-    Topic createTopic(Topic topic);
-}
